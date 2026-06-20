d3aae44ba5c92c8aff1baa3b9325617a18d3ef37
anhnd <anhndt0310@gmail.com>
2026-06-21 05:23:08 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
-BASE_DIR = '/content/drive/MyDrive'
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+if os.path.exists('/content/drive/MyDrive'):
+    BASE_DIR = '/content/drive/MyDrive/vietnamese_toxicity_detection_nlp'
 
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 MODEL_DIR = os.path.join(BASE_DIR, 'models')
 
-#Tự động tạo thư mục nếu chưa có
 os.makedirs(DATA_DIR, exist_ok=True)
 os.makedirs(MODEL_DIR, exist_ok=True)
