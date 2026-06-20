50782a334638a79bb1fe7fe6b5645831202a8fa0
anhnd <anhndt0310@gmail.com>
2026-06-21 02:09:24 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
 import os
-
-# Lấy đường dẫn của thư mục hiện tại chứa file này
 BASE_DIR = '/content/drive/MyDrive'
 
-# Các thư mục con sẽ nằm ngay trong thư mục dự án
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 MODEL_DIR = os.path.join(BASE_DIR, 'models')
 
-# Tự động tạo thư mục nếu chưa có
+#Tự động tạo thư mục nếu chưa có
 os.makedirs(DATA_DIR, exist_ok=True)
 os.makedirs(MODEL_DIR, exist_ok=True)
